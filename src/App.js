37a217695f05d2f2059217c3b8f7ccb9e8b92ebb
barbara06 <67117994+barbara06@users.noreply.github.com>
2021-02-16 15:01:38 +0100

    verifica2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-const ALLCATEGORIESURL = 'https://api.chucknorris.io/jokes/categories'
-const RANDOMJOKEBYCATURL = 'https://api.chucknorris.io/jokes/random?category=' // remember to fill this
+
 const ALLLJOKESBYKEYWORD = 'https://api.chucknorris.io/jokes/search?query=' // remember to fill this
 const launchErrorAlert = () => setTimeout(() => window.alert('errore!'), 500) 
 
@@ -13,32 +12,51 @@ const Logo = ({ loading }) => {
     <img
       src={logo}
       alt='interactive-logo'
-      // ... 
+      className={loading ? 'App-logo-spinning' : 'App-logo'}
     />
   )
 }
 
 const Joke = ({ value, categories }) => {
-  return null
-  // <div className="Joke">
-  //   <code className="Joke-Value">{value}</code>
-  //     per ciascun elemento di 'categories', renderizzare:
-  //     <span className="Selected-Cat" ... >
-  //       <code>{* QUI LA STRINGA DELLA SINGOLA CATEGORIA *}</code>
-  //     </span>
-  // </div>
+  return (
+    <div className="Joke">
+      <code className="Joke-Value">{value}</code>
+      {categories.map((category, index) => (
+        <span className="Selected-Cat" key={index} >
+          <code>{category}</code>
+        </span>
+      ))}
+    </div>
+  )  
 }
 
 // class App extends React.Component {
 function App() {
   // qui tutto ciò che serve al componente per essere inizializzato
+  const [inputText, setInputText] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [fetchedJoke, setFetchedJoke] = useState({})
 
   // getJokeByKeyword
   // funzione che recupera le barzellette contenenti la parola chiave
   // digitata nel campo di testo
+  const getJokeByKeyword = async () => {
+    let singleJoke = {}
+    try {
+      setLoading(true)
+      let response = await fetch(`${ALLLJOKESBYKEYWORD}${inputText}`)
+      let data = await response.json()
+      singleJoke= { ...data.result[0] }
+    } catch (error) {
+    } finally {
+      setFetchedJoke(singleJoke)
+      setLoading(false)
+    }
+  }
 
   // onInputTextChange
   // handler per l'input di testo
+const onInputTextChange = (event) => setInputText(event.target.value)
 
   // qui i lifecycle methods
 
@@ -47,17 +65,18 @@ function App() {
       <div className="App">
         <div className="App-header">
           <Logo
-            // ...
+            loading={loading}
           />
           <input
             type="search"
             id="search" name="search"
             placeholder="Enter keyword here"
-            // ...
+            value={inputText}
+            onChange={onInputTextChange}
           />
           <button
             className="Search-Button"
-            // ...
+            onClick={getJokeByKeyword}
           >
             <code>CLICK TO SEARCH!</code>
           </button>
@@ -68,10 +87,13 @@ function App() {
             className="Chuck-Logo"
             alt="chuck-logo"
           />
-          {/* <Joke ... /> */}
+          {Object.keys(fetchedJoke).length > 0 && <Joke
+            value={fetchedJoke.value}
+            categories={fetchedJoke.categories}
+          />}
         </div>
         <div className="footer">
-        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: </code>
+        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: Barbara Palazzo</code>
         </div>
       </div>
     );
